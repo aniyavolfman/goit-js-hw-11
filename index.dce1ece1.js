62facd7219949971f4d360882120993011fbafb0
aniyavolfman <115872174+aniyavolfman@users.noreply.github.com>
2023-01-09 22:19:07 +0000

    Deploying to gh-pages from @ aniyavolfman/goit-js-hw-11@f89bc318ae64053789be29180c0c40edc4484dc0 🚀

--- index.dce1ece1.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/?key=32728160-634e7d154d1682a06810c8278&q=flowers&image_type=photo&orientation=horizontal&safesearch=true").then((function(t){if(!t.ok)throw new Error(t.status);return t.json()})).then((function(t){return console.log(t.hits)})).catch((function(t){return console.log(t)}));
-//# sourceMappingURL=index.dce1ece1.js.map
